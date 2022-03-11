fca9404def2ccadf5044b9cd89ecf6c70cb24399
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-11 11:51:36 -0300

    everything is object

---
+++ everything-is-object/script-aula.js
@@ -0,0 +1,13 @@
+var nome = "caio";
+
+nome.length;
+nome.charAt(3);
+nome.replace("io", "kek");
+
+const altura = 1.75;
+
+altura.toFixed;
+
+var btn = document.querySelector(".btn");
+
+btn.classList.add();
---
+++ everything-is-object/script.js
@@ -0,0 +1,15 @@
+// nomeie 3 propriedades ou métodos de strings
+
+const nome = "caio";
+nome.charAt;
+nome.length;
+nome.replace;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn");
+
+btn.after;
+btn.append;
+btn.closest;
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
