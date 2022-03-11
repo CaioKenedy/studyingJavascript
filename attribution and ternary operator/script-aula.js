bb5933afdead44fe48cd0387c57b7e66f6c9b2a5
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-11 14:41:00 -0300

    ternary operator exercises

---
+++ attribution and ternary operator/script-aula.js
@@ -0,0 +1,17 @@
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+let numero = 10;
+numero = numero + 20;
+numero += 20;
+
+let idade = 25;
+let naoPossuiDiabetes = true;
+var legal = idade >= 18 && naoPossuiDiabetes ? "Pode beber" : "Não pode beber";
+console.log(legal);
